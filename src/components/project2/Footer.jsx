3aad490a087f70a5a82b9e658d2a4f5d752db70b
Nguyễn Minh Quân <85377764+TiredQuan@users.noreply.github.com>
2022-10-05 14:36:18 +0700

    a

--- src/components/project2/Footer.jsx
+++ src/components/project2/Footer.jsx
@@ -4,10 +4,10 @@ import './Footer.css'
 export default function Footer(){
     return(
     <div className='footer py-3 d-flex align-items-center justify-content-center gap-3'>
-        <a href="./Footer.jsx" className='link-dark'><Icon icon="fa:twitter-square" /></a>
-        <a href="./Footer.jsx" className='link-dark'><Icon icon="fa:twitter-square" /></a>
-        <a href="./Footer.jsx" className='link-dark'><Icon icon="fa:twitter-square" /></a>
-        <a href="./Footer.jsx" className='link-dark'><Icon icon="fa:twitter-square" /></a>
+        <a href="./Footer.jsx" className='link-light mx-1'><Icon className='f-size-25' icon="fa-brands:twitter-square" /></a>
+        <a href="./Footer.jsx" className='link-light mx-1'><Icon className='f-size-25' icon="fa-brands:facebook-square" /></a>
+        <a href="./Footer.jsx" className='link-light mx-1'><Icon className='f-size-25' icon="fa-brands:instagram-square" /></a>
+        <a href="./Footer.jsx" className='link-light mx-1'><Icon className='f-size-25' icon="fa-brands:github-square" /></a>
     </div>
-        )
+    )
 }
---
+++ src/components/project3/App.jsx
@@ -0,0 +1,13 @@
+import Header from './Header'
+import Main from './Main'
+import Footer from './Footer'
+import './App.css'
+
+
+export default function App(){
+    <div className="App">
+        <Header></Header>
+        <Main></Main>
+        <Footer></Footer>
+    </div>
+}
