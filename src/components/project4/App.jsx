03276c4809e78c83ff0910fca33f25fb7b19f1ad
Nguyễn Minh Quân <85377764+TiredQuan@users.noreply.github.com>
2022-10-08 11:56:19 +0700

    A

--- src/components/project4/App.jsx
+++ src/components/project4/App.jsx
@@ -1,5 +1,6 @@
 import styles from './App.module.css'
 import { Icon } from '@iconify/react'
+import TravelCard from './TravelCard'
 import data from './data.js'
 
 export default function App(){
@@ -9,6 +10,11 @@ export default function App(){
                 <Icon icon="fa6-solid:earth-americas" />
                 <p href="./App.jsx" className={styles.fw500fs14 + ' m-0'}>my travel journal.</p>
             </nav>
+            <section>
+                {data.map(e=>(
+                    <TravelCard key={e.id} {...e}></TravelCard>
+                ))}
+            </section>
         </div>
     )
 }
--- src/components/project4/TravelCard.jsx
+++ src/components/project4/TravelCard.jsx
@@ -1,7 +1,18 @@
 import styles from './TravelCard.module.css'
+import {Icon} from '@iconify/react'
 
-export default function Card(){
+
+
+export default function Card(props){
     return(
-        <></>
+        <div className={styles.TravelCard}>
+            <div className={styles.CardImage} style={{backgroundImage: `url(${props.imgSrc}`}}></div>
+            <div>
+                <p><Icon icon="jam:map-marker-f" /><span className='text-uppercase'>{props.country}</span><a target={'_blank'} rel={'noreferrer'} href={props.mapLink}>Place on Google Map</a></p>
+                <h3>{props.wonder}</h3>
+                <p>{props.date}</p>
+                <p>{props.desc}</p>
+            </div>
+        </div>
     )
 }
