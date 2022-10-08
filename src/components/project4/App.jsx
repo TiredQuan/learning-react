b9c2759525512635dd8ffdf072a0de61925d7c36
Nguyễn Minh Quân <85377764+TiredQuan@users.noreply.github.com>
2022-10-08 10:36:29 +0700

    tired and shit

--- src/components/project4/App.jsx
+++ src/components/project4/App.jsx
@@ -1,10 +1,14 @@
 import styles from './App.module.css'
-
+import { Icon } from '@iconify/react'
+import data from './data.js'
 
 export default function App(){
     return(
-        <div className={styles.apiCard}>
-
+        <div className={styles.App}>
+            <nav className={"text-center d-flex justify-content-center align-items-center"}>
+                <Icon icon="fa6-solid:earth-americas" />
+                <p href="./App.jsx" className={styles.fw500fs14 + ' m-0'}>my travel journal.</p>
+            </nav>
         </div>
     )
 }
---
+++ src/components/project4/TravelCard.jsx
@@ -0,0 +1,7 @@
+import styles from './TravelCard.module.css'
+
+export default function Card(){
+    return(
+        <></>
+    )
+}
