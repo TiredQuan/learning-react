3262741ee8d9bafa5960e833c810f7339cfc3a6a
Nguyễn Minh Quân <85377764+TiredQuan@users.noreply.github.com>
2022-10-10 10:10:23 +0700

    a

--- src/components/project4/App.jsx
+++ src/components/project4/App.jsx
@@ -6,7 +6,7 @@ import data from './data.js'
 export default function App(){
     return(
         <div className={styles.App}>
-            <nav className={`text-center d-flex justify-content-center align-items-center ${styles.nav}`}>
+            <nav className={`text-center d-flex justify-content-center align-items-center gap-1 ${styles.nav}`}>
                 <Icon icon="fa6-solid:earth-americas" />
                 <p href="./App.jsx" className={`${styles.fSize14} ${styles.fWeight500} m-0`}>my travel journal.</p>
             </nav>
