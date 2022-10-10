3520b70fb83ddd53043395e3325e559386fece57
Nguyễn Minh Quân <85377764+TiredQuan@users.noreply.github.com>
2022-10-10 19:08:23 +0700

    it works now

--- src/components/MainApp.jsx
+++ src/components/MainApp.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import App1 from './project1/App'
 import App2 from './project2/App'
 import App3 from './project3/App'
@@ -6,41 +7,42 @@ import styles from './MainApp.module.css'
 
 export default function MainApp(){
     const appArray = [App1,App2,App3,App4]
-    let CurrentApp
-    function handleSwitch(index){
-        switch (index)
+    let [arrayIndex, setArrayIndex] = useState()
+    let currentApp
+    // function handleSwitch(arrayIndex){
+        switch (arrayIndex)
             {
                 case 0:
-                    CurrentApp = App1
+                    currentApp = <App1/>
                     break
                 case 1:
-                    CurrentApp = App2
+                    currentApp = <App2/>
                     break
                 case 2:
-                    CurrentApp = App3
+                    currentApp = <App3/>
                     break
                 case 3:
-                    CurrentApp = App4
+                    currentApp = <App4/>
                     break
                 default:
-                    CurrentApp = ''
+                    currentApp = ''
                     break
             }
-    }
-    CurrentApp = App1
+    // }
     return(
         <div className={`container`}>
             <nav>
                 <ul className={`d-flex `}>
                     {appArray.map((e,index)=>(
                         <li key={index} className='list-group-item'>
-                            <button onClick={()=>handleSwitch(index)}>Project {index+1}</button>
+                            <button onClick={()=>setArrayIndex(arrayIndex = index)}>Project {index+1}</button>
+                            {/* <button onClick={()=>handleSwitch(index)}>Project {index+1}</button> */}
                             {/* The button is currently not rendering my inputs correctly, So I might revert it back to the way it used to be, with absolutely no onclicks and just project 4 */}
                         </li>
                     ))}
                 </ul>
             </nav>
-            <CurrentApp/>
+            {currentApp}
         </div>
     )
 }
