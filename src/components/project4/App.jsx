818cdc2c07a6cbaf50cbece8ab61f7dea0e8ad53
Nguyễn Minh Quân <85377764+TiredQuan@users.noreply.github.com>
2022-10-10 10:06:25 +0700

    project 3 done

--- src/components/project4/App.jsx
+++ src/components/project4/App.jsx
@@ -6,11 +6,11 @@ import data from './data.js'
 export default function App(){
     return(
         <div className={styles.App}>
-            <nav className={"text-center d-flex justify-content-center align-items-center"}>
+            <nav className={`text-center d-flex justify-content-center align-items-center ${styles.nav}`}>
                 <Icon icon="fa6-solid:earth-americas" />
-                <p href="./App.jsx" className={styles.fw500fs14 + ' m-0'}>my travel journal.</p>
+                <p href="./App.jsx" className={`${styles.fSize14} ${styles.fWeight500} m-0`}>my travel journal.</p>
             </nav>
-            <section>
+            <section className={`mt-5`}>
                 {data.map(e=>(
                     <TravelCard key={e.id} {...e}></TravelCard>
                 ))}
--- src/components/project4/TravelCard.jsx
+++ src/components/project4/TravelCard.jsx
@@ -1,3 +1,4 @@
+import Appstyles from './App.module.css'
 import styles from './TravelCard.module.css'
 import {Icon} from '@iconify/react'
 
@@ -5,13 +6,15 @@ import {Icon} from '@iconify/react'
 
 export default function Card(props){
     return(
-        <div className={styles.TravelCard}>
+        <div className={`${styles.TravelCard} w-100 d-flex gap-3 align-items-center justify-content-center my-4`}>
             <div className={styles.CardImage} style={{backgroundImage: `url(${props.imgSrc}`}}></div>
-            <div>
-                <p><Icon icon="jam:map-marker-f" /><span className='text-uppercase'>{props.country}</span><a target={'_blank'} rel={'noreferrer'} href={props.mapLink}>Place on Google Map</a></p>
-                <h3>{props.wonder}</h3>
-                <p>{props.date}</p>
-                <p>{props.desc}</p>
+            <div className={styles.CardInfo}>
+                <p className={`${Appstyles.fSize10} mb-1 d-flex`}><Icon icon="jam:map-marker-f" className={`${styles.icon}`}/><span className={`text-uppercase ${styles.textCountry} me-2`}>{props.country}</span><a target={'_blank'} rel={'noreferrer'} href={props.mapLink}>Place on Google Map</a></p>
+                <h3 className={`${Appstyles.fWeight700} ${Appstyles.fSize25} mb-3`}>{props.wonder}</h3>
+                <div className={`${Appstyles.fSize10}`}>
+                    <p className={`${Appstyles.fWeight700} mb-1`}>{props.date}</p>
+                    <p className={`${Appstyles.fWeight400} mb-1`}>{props.desc}</p>
+                </div>
             </div>
         </div>
     )
