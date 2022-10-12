e76e9ba4733afb333230b9fd8a3e6ce382cabac7
Nguyễn Minh Quân <85377764+TiredQuan@users.noreply.github.com>
2022-10-12 17:18:40 +0700

    done the basic functions

--- src/components/project5/Main.jsx
+++ src/components/project5/Main.jsx
@@ -9,22 +9,32 @@ export default function Main(){
 
 
 
-    const [memeImage, setMemeImage] = useState("")
+    const imgArray = data.memes
+    const [memeImage, setMemeImage] = useState(imgArray[Math.floor(Math.random()*imgArray.length)].url)
+    const [cringeValue1,setCringeValue1] = useState()
+    const [cringeValue2,setCringeValue2] = useState()
 
+    let cringeInput1 = document.getElementById('cringe1'),cringeInput2 = document.getElementById('cringe1')
+    function submitCringe(){
+        setCringeValue1(cringeInput1.value)
+        setCringeValue2(cringeInput2.value)
+    }
 
+    
     function getImage(){
-        const imgArray = data.memes
         const randomNumber = Math.floor(Math.random()*imgArray.length)
         return setMemeImage(prevImage => prevImage = imgArray[randomNumber].url)
     }
     return(
         <div className={`${styles.Main} px-4 w-100`}>
             <div className='input-group py-5'>
-                <input className={`${gStyles.fWeight400} ${gStyles.fSize12} form-control`} placeholder='text1' type="text" />
-                <input className={`${gStyles.fWeight400} ${gStyles.fSize12} form-control`} placeholder='text2' type="text" />
+                <input id='cringe1' className={`${gStyles.fWeight400} ${gStyles.fSize12} form-control`} placeholder='text1' type="text" />
+                <input id='cringe2' className={`${gStyles.fWeight400} ${gStyles.fSize12} form-control`} placeholder='text2' type="text" />
             </div>
-            <div className='d-grid py-3'><button className={`${styles.btnCustom} btn ${gStyles.fWeight700} ${gStyles.fSize16}`} onClick={getImage}>Get a new image</button></div>
+            <div className='d-grid py-3'><button className={`${styles.btnCustom} btn ${gStyles.fWeight700} ${gStyles.fSize16}`} onClick={()=>{getImage(); submitCringe()}}>Get a new image</button></div>
+            <p>{cringeValue1}</p>
             <img className='w-100 ' src={memeImage} alt="" />
+            <p>{cringeValue2}</p>
         </div>
     )
 }
