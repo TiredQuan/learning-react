e96eb59b9eb5aef5473bf6339b8da6c10917787d
Nguyễn Minh Quân <85377764+TiredQuan@users.noreply.github.com>
2022-10-18 17:23:40 +0700

    a

--- src/components/project5/App.jsx
+++ src/components/project5/App.jsx
@@ -9,7 +9,6 @@ export default function App(){
 
     return(
         <div className={`${styles.App} mx-auto`}>
-            <p>Yeah I know it looks like Shit I don't care I learnt how to use useState, that's all that matters fuck you</p>
             <Header></Header>
             <Main></Main>
         </div>
--- src/components/project5/Main.jsx
+++ src/components/project5/Main.jsx
@@ -1,7 +1,6 @@
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 import gStyles from './App.module.css'
 import styles from './Main.module.css'
-import data from './data'
 
 
 
@@ -9,34 +8,38 @@ export default function Main(){
 
 
 
-    const imgArray = data.memes
+    const [imgArray,setImgArray] = useState()
+
+    useEffect(()=>{
+        fetch('https://api.imgflip.com/get_memes')
+        .then(res => res.json())
+        .then(data => setImgArray(data.data.memes))
+    }, [])
+    
     const [cringeGod, setCringeGod] = useState({
-        memeImage: imgArray[Math.floor(Math.random()*imgArray.length)].url,
+        memeImage: "https://i.imgflip.com/1bij.jpg",
         cringeValue1: 'Sample Text',
         cringeValue2: 'Sample Text',
     }
     )
-
-    let cringeInput1 = document.getElementById('cringe1'),cringeInput2 = document.getElementById('cringe2')
-    function submitCringe(){
-        if(cringeInput1.value === '' || cringeInput2.value === ''){
-            setCringeGod(prefCringe => ({
+    function submitCringe(event){
+        if(event.target.value === ''){
+            return setCringeGod(prefCringe => ({
                 ...prefCringe,
-                cringeValue1:'Sample Text',
-                cringeValue2:'Sample Text',
+                [event.target.name]:'Sample Text',
             }))
         } else{
-            setCringeGod(prefCringe => ({
+            return setCringeGod(prefCringe => ({
                 ...prefCringe,
-                cringeValue1:cringeInput1.value,
-                cringeValue2:cringeInput2.value,
+                [event.target.name]:event.target.value,
             }))
         }
     }
     
+
     function getImage(){
         const randomNumber = Math.floor(Math.random()*imgArray.length)
-        setCringeGod(prefCringe => ({
+        return setCringeGod(prefCringe => ({
             ...prefCringe,
             memeImage:imgArray[randomNumber].url,
         }))
@@ -44,15 +47,17 @@ export default function Main(){
     return(
         <div className={`${styles.Main} px-4 w-100`}>
             <div className='input-group py-5'>
-                <input id='cringe1' className={`${gStyles.fWeight400} ${gStyles.fSize12} form-control`} placeholder='text1' type="text" />
-                <input id='cringe2' className={`${gStyles.fWeight400} ${gStyles.fSize12} form-control`} placeholder='text2' type="text" />
+                <input id='cringe1' onChange={submitCringe} name='cringeValue1' className={`${gStyles.fWeight400} ${gStyles.fSize12} form-control`} placeholder='text1' type="text" />
+                <input id='cringe2' onChange={submitCringe} name='cringeValue2' className={`${gStyles.fWeight400} ${gStyles.fSize12} form-control`} placeholder='text2' type="text" />
             </div>
-            <div className={`d-grid py-3 ${styles.cringer}`}>
-                <button className={`${styles.btnCustom} btn ${gStyles.fWeight700} ${gStyles.fSize16}`} onClick={()=>{getImage(); submitCringe()}}>Get a new image</button>
+            <div className={`d-grid py-3`}>
+                <button className={`${styles.btnCustom} btn ${gStyles.fWeight700} ${gStyles.fSize16}`} onClick={()=>{getImage()}}>Get a new image</button>
                 </div>
-            <p className={`${styles.cringeCarrier} ${styles.cringeCarrier1}`}>{cringeGod.cringeValue1}</p>
-            <img className='w-100 ' src={cringeGod.memeImage} alt="" />
-            <p className={`${styles.cringeCarrier} ${styles.cringeCarrier2}`}>{cringeGod.cringeValue2}</p>
+            <div className={` ${styles.cringer}`}>  
+                <p className={`${styles.cringeCarrier} ${styles.cringeCarrier1}`}>{cringeGod.cringeValue1}</p>
+                <img className='w-100 ' src={cringeGod.memeImage} alt="" />
+                <p className={`${styles.cringeCarrier} ${styles.cringeCarrier2}`}>{cringeGod.cringeValue2}</p>
+            </div>
         </div>
     )
 }
